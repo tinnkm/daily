ff3d8de4a93828063f7d261dd77760b7ac02378d
tinnkm <tinnkm@gmail.com>
2018-07-20 17:56:55 +0800

    init

---
+++ src/main/java/com/tinnkm/daily/day01/Question.java
@@ -0,0 +1,26 @@
+/*
+ * @project: daily
+ * @package: com.tinnkm.daily.day01
+ * @filename: Question
+ *
+ * Copyright (c) 2018 eSunny Info. Tech Ltd. All rights reserved.
+ *
+ */
+package com.tinnkm.daily.day01;
+
+/**
+ * @typename: Question
+ * @brief: 〈一句话功能简述〉
+ * @author: quantdo_wh02
+ * @date: 2018/7/20 17:49
+ * @version: 1.0.0
+ * @since
+ */
+public class Question {
+    /**
+     *
+     * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+     *
+     * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+     */
+}
---
+++ src/main/java/com/tinnkm/daily/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * @project: daily
+ * @package: com.tinnkm.daily
+ * @filename: package-info
+ *
+ * Copyright (c) 2018 eSunny Info. Tech Ltd. All rights reserved.
+ *
+ */
+package com.tinnkm.daily;
